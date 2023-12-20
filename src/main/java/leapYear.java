1e38e3b45d624a1d96f42ebfbb15fce880196d22
Suresh1925 <137748383+Suresh1925@users.noreply.github.com>
2023-12-20 16:02:56 +0530

    Update leapYear.java

--- src/main/java/leapYear.java
+++ src/main/java/leapYear.java
@@ -1,6 +1,6 @@
 public class my_first {
     public static void main(String[] args){
-        int year=Integer.parseInt(args[0]);
+        int year=2026;
         if(year%4==0){
             System.out.println("It is a Leap yaer");
         }
